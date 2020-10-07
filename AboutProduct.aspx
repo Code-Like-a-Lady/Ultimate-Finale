7dc5f8ed88f9863e403acd24ae1c86cf7ad1088e
LaaiqahSeedat <seedatlaaiqah@gmail.com>
2020-10-07 10:46:48 +0200

    Added all files to repository

---
+++ AboutProduct.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MaskInc.Master" AutoEventWireup="true" CodeBehind="AboutProduct.aspx.cs" Inherits="Group_MaskInc_FrontEnd.WebForm16" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+      <h1><i class="fa fa-cart-plus"></i>&nbsp;About Product</h1>
+    <hr />
+    <section class="product-page">
+        <div class="container">
+            <div class="product-control">
+                <a href="Catalog.aspx">Previous</a>
+                <a href="CheckOut.aspx">Next</a>
+            </div>
+            <!-- Image will appear in here -->
+                <div id="pimage" runat="server">
+
+             </div>
+   <div class="mt-10" id="btn_AddtoCart" runat="server">
+       <a href="#" runat="server" class="site-btn" name="AddToCart" onserverclick="Unnamed_ServerClick">Add to Cart</a>
+    </div>
+    <div class="mt-10" id="btn_removeProduct" runat="server">
+         <a href="#" runat="server" class="site-btn" onserverclick="Unnamed_ServerClick1">Remove Product</a>
+    </div>
+        </div>                      
+    </section>
+
+     <section class="information">
+         <!-- product information will come in here -->
+                <div id="prod" runat="server">
+
+             </div>
+    </section>
+</asp:Content>
---
+++ AboutProduct.aspx.cs
@@ -0,0 +1,71 @@
+﻿using Group_MaskInc_FrontEnd.GroupServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Group_MaskInc_FrontEnd
+{
+    public partial class WebForm16 : System.Web.UI.Page
+    {
+        GroupServiceClient sr = new GroupServiceClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int ID = Convert.ToInt32(Session["LoggedInUserID"]);
+            var user = sr.GetUser(ID);
+            if (Session["LoggedInUserID"] != null)
+            {
+                if (user.Usertype.Equals("client"))
+                {
+                    btn_removeProduct.Visible = false;
+                   
+                }
+                else if (user.Usertype.Equals("admin"))
+                {
+                    btn_AddtoCart.Visible = false;
+                }
+            }
+
+            var itemid = Request.QueryString["ID"];
+            var mask = sr.GetProduct(Convert.ToInt32(itemid));
+
+            string displayi = " ";
+            string display = " ";
+
+            displayi += "<center><img src='" + mask.Image + "' alt='" + mask.Name + "' class='img-fluid'></center>";
+            display += "<center><h1>" + mask.Name + "</h1><p>" + mask.Description + "</p><p> R" + Math.Round(mask.Unit_Price, 2) + "</p></center>";
+            pimage.InnerHtml = displayi;
+            prod.InnerHtml = display;
+
+        }
+
+        
+        //Add to cart click
+        protected void Unnamed_ServerClick(object sender, EventArgs e)
+        {
+            //getting the user ifm
+            int ID = Convert.ToInt32(Session["LoggedInUserID"]);
+            ID = 9;
+            //getting the product id
+            var itemid = Request.QueryString["ID"];
+            var id = Convert.ToInt32(itemid);
+            var mask = sr.GetProduct(Convert.ToInt32(itemid));
+
+            //adding to cart
+            bool add = sr.AddtoCart(ID, mask.Product_Id, 50, mask.Unit_Price * 50);
+        }
+
+        //Removing a product
+        protected void Unnamed_ServerClick1(object sender, EventArgs e)
+        {
+            //getting the user ifm
+            int ID = Convert.ToInt32(Session["LoggedInUserID"]);
+
+            //getting the product
+            var itemid = Request.QueryString["ID"];
+            Response.Redirect("RemoveProduct.aspx?ID=" + itemid);
+        }
+    }
+}
